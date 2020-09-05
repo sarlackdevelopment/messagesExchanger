a7cccbae06cfb64e543af684ff1070c08ca9bece
Korobov Alexandr <askorobov@vtb.ru>
2020-09-05 14:05:06 +0300

    initialization

---
+++ src/main/java/Player.java
@@ -0,0 +1,24 @@
+public class Player {
+
+    public Player(int massagesCounter) {
+        this.massagesCounter = massagesCounter;
+    }
+
+    public int getMassagesCounter() {
+        return massagesCounter;
+    }
+
+    public void setMassagesCounter(int massagesCounter) {
+        this.massagesCounter = massagesCounter;
+    }
+
+    private int massagesCounter;
+
+    public void send() {
+
+    }
+
+    public void receive() {
+
+    }
+}
